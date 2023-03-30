5d555c1c5e7e42ffceb1f7edf2d4f507f7ba85e2
lucaslozz <lucass.mark@gmail.com>
2023-03-30 10:13:35 -0300

    adiciona feature para bloquear tasks vazias

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -12,7 +12,8 @@ export function Input(){
   const [allTasks, setAllTasks]=useState<AllTasks[]>([])
   const [newTask, setNewTask]=useState<AllTasks>({id:"",content:"",isChecked:false})
 
-  console.log(allTasks)
+
+  
   function handleChange(event:ChangeEvent<HTMLTextAreaElement>){
     let addNewTask = {
       id: uuidv4(),
@@ -20,13 +21,14 @@ export function Input(){
       isChecked:false
     }
     setNewTask(addNewTask)
+  
   }
 
 
   function handleSubmit(event:FormEvent<HTMLFormElement>){
     event.preventDefault();
     setAllTasks([...allTasks,...[newTask]])
-    setNewTask({id:"",content:"",isChecked:false})    
+    setNewTask({id:"",content:"",isChecked:false})
   }
 
   function deleteTask(taskToDelete:AllTasks){
@@ -50,8 +52,8 @@ export function Input(){
   return( 
       <div>
         <form className={styles.formContainer} onSubmit={handleSubmit} >
-          <textarea name="input" placeholder="Adicione uma nova tarefa" onChange={handleChange} value={newTask.content} />
-          <button type="submit" >Criar <PlusCircle size={16} weight="bold"/></button>
+          <textarea name="input" placeholder="Adicione uma nova tarefa" onChange={handleChange} value={newTask.content}  />
+          <button type="submit" disabled={newTask.content.length === 0} >Criar <PlusCircle size={16} weight="bold"/></button>
         </form>
          <div>
           <TaskBoard content={allTasks} deleteTask={deleteTask} refreshTaskStatus={refreshTaskStatus}/>
